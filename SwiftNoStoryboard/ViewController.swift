85a046c51a1103b21da798be294efa1f5b90dabf
benben <benben>
2016-01-03 20:18:43 -0600

    First

---
+++ SwiftNoStoryboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftNoStoryboard
+//
+//  Created by benjaminhallock@gmail.com on 1/3/16.
+//  Copyright © 2016 ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Main View"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
